829f3569fce6b4d38ceac3e5d110a96f2b4a9fc9
Topuria <36174597+topuria@users.noreply.github.com>
2020-11-11 19:03:57 +0400

    Update CalculatorService.java

--- nodar-topuria-v1/service/CalculatorService.java
+++ nodar-topuria-v1/service/CalculatorService.java
@@ -1,9 +1,7 @@
-package ge.edu.calculator.service;
+package ge.edu.CurrencyConverter.service;
 
-public interface CalculatorService {
-     void sum(int x, int y);
-     void divide(int x,int y);
-
-     void sum(double x,double y);
-     void divide(double x, double y);
+public interface CurrencyService {
+     void convert(int amount);
+     void convert(double amount);
+     void setExchangeRate(double rate);
 }
