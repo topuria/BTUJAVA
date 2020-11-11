140dbe3efbb2352272b4598b1727b1f8b54bd19d
Topuria <36174597+topuria@users.noreply.github.com>
2020-11-11 18:58:54 +0400

    Create CalculatorService.java

---
+++ nodar-topuria-v1/service/CalculatorService.java
@@ -0,0 +1,9 @@
+package ge.edu.calculator.service;
+
+public interface CalculatorService {
+     void sum(int x, int y);
+     void divide(int x,int y);
+
+     void sum(double x,double y);
+     void divide(double x, double y);
+}
